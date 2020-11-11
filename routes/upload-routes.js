9137360ac78c056a9d4d2a44295cbe6e0092f77b
Mohammed Rezq <mohammedrezq2000@gmail.com>
2020-11-11 21:55:56 +0200

    Added new route for uploading multiple images for new products form

--- routes/upload-routes.js
+++ routes/upload-routes.js
@@ -35,17 +35,17 @@ function checkFileType (file, cb) {
 }
 
 
-const upload = multer({
-    storage,
-    limits: {
-        fileSize: 12000000,
-    },
-    fileFilter: (req, file, cb) => {
-       checkFileType(file, cb)
-    }
-});
+// const upload = multer({
+//     storage,
+//     limits: {
+//         fileSize: 12000000,
+//     },
+//     fileFilter: (req, file, cb) => {
+//        checkFileType(file, cb)
+//     }
+// });
 
-router.post('/', upload.single('Images'), (req, res, next) => {
+// router.post('/', upload.single('Images'), (req, res, next) => {
     // const file = req.file.path // Single Image
     // console.log(file)
 
@@ -61,13 +61,33 @@ router.post('/', upload.single('Images'), (req, res, next) => {
     // console.log("THE PATHS",paths)
     // console.log(paths)
 
-    const file = req.file;
-    file.path = file.path.replace(/\\/g, "/"); // make sure image link is OK slashed from: https://stackoverflow.com/questions/59393275/multer-req-files-path-not-editable
+//     const file = req.file;
+//     file.path = file.path.replace(/\\/g, "/"); // make sure image link is OK slashed from: https://stackoverflow.com/questions/59393275/multer-req-files-path-not-editable
 
-    res.send(`/${file.path}`)
+//     res.send(`/${file.path}`)
+// })
+
+
+// Korean
+const theupload = multer({
+    storage,
+    limits: {
+        fileSize: 120000000,
+    },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb)
+    }
+}).single("file");
+
+// Add protect and adminstrator (Korean)
+router.post('/uploadImage' ,(req, res, next) => {
+
+    theupload(req, res, err => {
+        if(err) return res.json({success:false, err})
+        const imagePath = res.req.file.path.replace(/\\/g, "/"); // make sure image link is OK slashed from: https://stackoverflow.com/questions/59393275/multer-req-files-path-not-editable
+        return res.json({success: true, image: `http://localhost:5000/${imagePath}`, fileName: res.req.file.filename })
+    })
+
 })
-
-
-
 
 module.exports = router;
